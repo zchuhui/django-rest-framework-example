46f3265abfaf3895c2447b2e5c74b4d7e167f956
zhuangchuhui <zhuangchuhui@iunispace.com>
2021-11-27 17:49:14 +0800

    删除无关文件夹

--- .history/myapi/admin_20211127165332.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Hero,Company
-
-admin.site.register(Hero)
-admin.site.register(Company)
-
--- .history/myapi/models_20211127162844.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Hero Model 
-class Hero(models.Model):
-    name = models.CharField(max_length=60)
-    alias = models.CharField(max_length=60)
-    
-    def __str__(self):
-        return self.name
